37af7f43f234511f928833a91546937f61ccc4aa
PaulwDuffy <47083890+PaulwDuffy@users.noreply.github.com>
2020-06-04 17:03:53 +0100

    ShoppingCart

---
+++ AnkleBitersCreche1/Models/ServiceDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnkleBitersCreche1.Models
+{
+    public class ServiceDetails
+    {
+        public int Id { get; set; }
+
+        public int ServiceHeaderId { get; set; }
+        [ForeignKey("ServiceHeaderId")]
+        public virtual ServiceHeader ServiceHeader { get; set; }
+
+        [Display(Name = "Service")]
+        public int OurServiceId { get; set; }
+        [ForeignKey("OurServiceId")]
+        public virtual OurService OurService { get; set; }
+
+        public double ServicePrice { get; set; }
+        public string ServiceName { get; set; }
+    }
+}
---
+++ AnkleBitersCreche1/Models/ViewModel/StudentServiceViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnkleBitersCreche1.Models.ViewModel
+{
+    public class StudentServiceViewModel
+    {
+        public Student Student { get; set; }
+        public ServiceHeader ServiceHeader { get; set; }
+        public ServiceDetails ServiceDetails { get; set; }
+
+        public List<OurService> OurServiceList { get; set; }
+        public List<ServiceShoppingCart> ServiceShoppingCart { get; set; }
+    }
+}
